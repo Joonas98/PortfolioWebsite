fe4f7babc94449ef46e6608656de3e1f634c70ab
Joonas <jsuuronen9@gmail.com>
2025-03-30 20:10:55 +0300

    Slideshow added

--- Models/Project.cs
+++ Models/Project.cs
@@ -8,5 +8,6 @@
 		public string YearRange { get; set; } = string.Empty;
 		public string RepoUrl { get; set; } = string.Empty; // Optional GitHub link
 		public string OtherLink { get; set; } = string.Empty; // Optional link to other site
+		public List<string> ImageUrls { get; set; } = new();
 	}
 }
