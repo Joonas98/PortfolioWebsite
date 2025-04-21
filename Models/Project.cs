52e799b62e2e759e90eeacb6e0aae602d41492be
Joonas <jsuuronen9@gmail.com>
2025-04-21 18:47:57 +0300

    Major content additions and ugprades.

--- Models/Project.cs
+++ Models/Project.cs
@@ -6,8 +6,19 @@
 		public string Description { get; set; } = string.Empty;
 		public List<string> Technologies { get; set; } = new();
 		public string YearRange { get; set; } = string.Empty;
-		public string RepoUrl { get; set; } = string.Empty; // Optional GitHub link
-		public string OtherLink { get; set; } = string.Empty; // Optional link to other site
-		public List<string> ImageUrls { get; set; } = new();
+		public List<Link> Links { get; set; } = new();
+		public List<ImageInfo> Images { get; set; } = new();
+
+		public class Link
+		{
+			public string Text { get; set; } = string.Empty;
+			public string Url { get; set; } = string.Empty;
+		}
+
+		public class ImageInfo
+		{
+			public string Url { get; set; } = string.Empty;
+			public string Alt { get; set; } = string.Empty;
+		}
 	}
 }
